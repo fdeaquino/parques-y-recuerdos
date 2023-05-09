554ad021cacf2dc35d6f82e025c0475d54fbda0e
FDeaquino <fidel.deaquino@gmail.com>
2023-05-09 17:11:12 -0500

    added geolocations to geoData and imported that data into InteractiveMap.js, also added as much of a 3D effect as I could with pitch and bearing values

--- src/components/InteractiveMap/InteractiveMap.js
+++ src/components/InteractiveMap/InteractiveMap.js
@@ -1,25 +1,63 @@
 import React, { useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
+import { geoData } from './geoData'; // import geoData
 
+
+// Set the Mapbox access token from environment variable
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
 const InteractiveMap = () => {
+    // Uses the useRef hook to create a ref to attach to the map container div
     const mapContainerRef = useRef(null);
 
+    // Uses the useEffeect hook to run this code once after the component mounts
     useEffect(() => {
+        // Create a new Mapbox map object
+        // Attach it to the map container div, and set some map options
         const map = new mapboxgl.Map({
-            container: mapContainerRef.current, 
-            style: 'mapbox://styles/mapbox/streets-v11', 
-            center: [-104.9876, 39.7405],
-            zoom: 9,
+            container: mapContainerRef.current,
+            style: 'mapbox://styles/mapbox/outdoors-v12',
+            center: [-95.4540, 29.766],
+            zoom: 14,
+            pitch: 60, // pitch in degrees
+            bearing: -60 // bearing in degrees
+        });
+        map.on('load', () => {
+            // Iterate over the geoData features
+            geoData.features.forEach((feature) => {
+                // Add the source for each feature
+                map.addSource(feature.properties.name, {
+                    type: 'geojson',
+                    data: feature,
+                });
+
+                // Add a layer for each source
+                map.addLayer({
+                    id: feature.properties.name,
+                    type: 'line', // change this as per your needs
+                    source: feature.properties.name,
+                    layout: {
+                        'line-join': 'round',
+                        'line-cap': 'round'
+                    },
+                    paint: {
+                        'line-color': '#FF4F00',
+                        'line-width': 5
+                    }
+                });
+            });
         });
 
+        // Add navigation controls to the map
         map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
 
+        // Clean up on unmount by removing the map
         return () => map.remove();
-    }, []);
+    }, []); // Empty dependency array means this effect runs once on mount and cleanup on unmount
 
-    return <div ref={mapContainerRef} style={{ width: '100%', height: '100vh' }} />
+    // Render a div that will hold the map
+    // The ref created is attached here
+    return <div ref={mapContainerRef} style={{ width: '40%', height: '100vh' }} />
 };
 
 export default InteractiveMap;
