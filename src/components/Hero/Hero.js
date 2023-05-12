a51e8b9b834f6ce786650ff1fe1bc3c34f7013eb
FDeaquino <fidel.deaquino@gmail.com>
2023-05-12 10:42:53 -0500

    fixed map to background and positioned Hero and BlogPost to overlay on top of the Map, changed sentence in Hero intro-paragraph

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,7 +12,7 @@ function Hero() {
                     Bilingual Reflections on Park Adventures
                 </h2>
                 <p className='intro-paragraph'>
-                    Como beneficiario de DACA, I've been limited to travel within the U.S., pero encontré alegría explorando local, state, y national parks. Hiking with Ross, encontramos una conexión profunda con la naturaleza, nurturing our respect for the Earth. The physical and psychological benefits of hiking son enormes, bringing us closer to understanding la diversidad cultural y la rica historia de los pueblos originarios whose lands we tread upon.  In every breath of fresh air, estamos agradecidos por la oportunidad de descubrir, aprender y crecer.
+                    Como beneficiario de DACA, I've been limited to travel within the U.S., pero encontré alegría explorando local, state, y national parks. My husband and hiking partner, Ross, y yo hemos encontrado una conexión profunda con la naturaleza, nurturing our respect for the Earth. The physical and psychological benefits of hiking son enormes, bringing us closer to understanding la diversidad cultural y la rica historia de los pueblos originarios whose lands we tread upon.  In every breath of fresh air, estamos agradecidos por la oportunidad de descubrir, aprender y crecer.
                 </p>
 
 
